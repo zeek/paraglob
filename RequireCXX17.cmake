97cca9c8c6ab0f4709cd2050ebd7d1c1583cd4dc
Robin Sommer <robin@corelight.com>
2019-10-28 10:07:31 +0000

    Switch to require C++17.

--- RequireCXX17.cmake
+++ RequireCXX17.cmake
@@ -1,38 +1,27 @@
-# Detect if compiler version is sufficient for supporting C++11.
-# If it is, CMAKE_CXX_FLAGS are modified appropriately and HAVE_CXX11
+# Detect if compiler version is sufficient for supporting C++17.
+# If it is, CMAKE_CXX_FLAGS are modified appropriately and HAVE_CXX17
 # is set to a true value.  Else, CMake exits with a fatal error message.
 # This currently only works for GCC and Clang compilers.
-# In Cmake 3.1+, CMAKE_CXX_STANDARD_REQUIRED should be able to replace
+# In Cmake 3.8+, CMAKE_CXX_STANDARD_REQUIRED should be able to replace
 # all the logic below.
 
-# This is a copy from zeek/cmake/RequireCXX11.cmake. Rather than including the
-# entire subdirectory, we thought it would be cleaner to just move this one
-# file over.
-
-if ( DEFINED HAVE_CXX11 )
+if ( DEFINED HAVE_CXX17 )
     return()
 endif ()
 
 include(CheckCXXSourceCompiles)
 
-set(required_gcc_version 4.8)
-set(required_clang_version 3.3)
+set(required_gcc_version 7.0)
+set(required_clang_version 4.0)
 
-macro(cxx11_compile_test)
-    # test a header file that has to be present in C++11
+macro(cxx17_compile_test)
     check_cxx_source_compiles("
-    #include <array>
-    #include <iostream>
-        int main() {
-            std::array<int, 2> a{ {1, 2} };
-            for (const auto& e: a)
-                std::cout << e << ' ';
-            std::cout << std::endl;
-            }
-        " cxx11_header_works)
+        #include <optional>
+        int main() { std::optional<int> a; }"
+        cxx17_works)
 
-    if (NOT cxx11_header_works)
-        message(FATAL_ERROR "C++11 headers cannot be used for compilation")
+    if (NOT cxx17_works)
+        message(FATAL_ERROR "failed using C++17 for compilation")
     endif ()
 endmacro()
 
@@ -42,21 +31,21 @@ if ( DEFINED CMAKE_CXX_COMPILER_VERSION )
     if ( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
         if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${required_gcc_version} )
             message(FATAL_ERROR "GCC version must be at least "
-                    "${required_gcc_version} for C++11 support, detected: "
+                    "${required_gcc_version} for C++17 support, detected: "
                     "${CMAKE_CXX_COMPILER_VERSION}")
         endif ()
     elseif ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
         if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS ${required_clang_version} )
             message(FATAL_ERROR "Clang version must be at least "
-                    "${required_clang_version} for C++11 support, detected: "
+                    "${required_clang_version} for C++17 support, detected: "
                     "${CMAKE_CXX_COMPILER_VERSION}")
         endif ()
     endif ()
 
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-    cxx11_compile_test()
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+    cxx17_compile_test()
 
-    set(HAVE_CXX11 true)
+    set(HAVE_CXX17 true)
     return()
 endif ()
 
@@ -66,14 +55,22 @@ if ( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
                     gcc_version)
     if ( ${gcc_version} VERSION_LESS ${required_gcc_version} )
         message(FATAL_ERROR "GCC version must be at least "
-                "${required_gcc_version} for C++11 support, manually detected: "
+                "${required_gcc_version} for C++17 support, manually detected: "
                 "${CMAKE_CXX_COMPILER_VERSION}")
     endif ()
 elseif ( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
-    # TODO: don't seem to be any great/easy ways to get a clang version string.
+    execute_process(COMMAND ${CMAKE_CXX_COMPILER} -v ERROR_VARIABLE
+                    clang_version)
+    string(REGEX REPLACE "^clang version ([^ ]+) .*" "\\1"
+           clang_version "${clang_version}")
+    if ( ${clang_version} VERSION_LESS ${required_clang_version} )
+        message(FATAL_ERROR "GCC version must be at least "
+                "${required_clang_version} for C++17 support, manually detected: "
+                "${CMAKE_CXX_COMPILER_VERSION}")
+    endif ()
 endif ()
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-cxx11_compile_test()
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+cxx17_compile_test()
 
-set(HAVE_CXX11 true)
+set(HAVE_CXX17 true)
